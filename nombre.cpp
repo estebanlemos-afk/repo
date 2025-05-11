3e503c80d0e42d364aefe7f97084ec4f7167d6e9
Esteban lemos <juan.lemos.clavijo@correounivalle.edu.co>
2025-05-11 16:25:34 -0500

    materia

--- nombre.cpp
+++ nombre.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
     cout<<"Juan Esteban Lemos Clavijo"<<endl; 
     cout<<"codigo 202419266"<<endl;
+    cout<<"Metodologias Desarrollo de Software"<<endl;
     return 0;
 }
